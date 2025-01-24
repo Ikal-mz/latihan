ab3e8762d0af3e05f0a693c26b1acc11f15d5a70
Uda Mz <mzulfikarb11@gmail.com>
2025-01-24 20:57:59 +0900

    Update 48

--- .vscode/EP 48 - LATIHAN FUNGSI/Main.py
+++ .vscode/EP 48 - LATIHAN FUNGSI/Main.py
@@ -3,7 +3,7 @@
 import os
 
 # membuat header program
-os.system("cls")
+"""os.system("cls")
 
 print(f"{'PROGRAM MENGHITUNG LUAS':^40}")
 print(f"{'DAN KELILING PERSEGI PANJANG':^40}")
@@ -19,8 +19,45 @@ KELILING = 2*(PANJANG+LEBAR)
 
 # tampilkan hasilnya
 print(f"Hasil perhitungan luas : {LUAS}")
-print(f"Hasil perhitungan keliling : {KELILING}")
+print(f"Hasil perhitungan keliling : {KELILING}")"""
 
 
-## LANJUTKAN 
-# https://www.youtube.com/watch?v=AcyUE59S53U&list=PLZS-MHyEIRo59lUBwU-XHH7Ymmb04ffOY&index=51&ab_channel=KelasTerbuka
+def header() :
+    """FUNGSI HEADER"""
+    os.system("cls")
+    print(f"{'PROGRAM MENGHITUNG LUAS':^40}")
+    print(f"{'DAN KELILING PERSEGI PANJANG':^40}")
+    print(f"{'-'*30:^40}")
+
+def input_user() : 
+    """FUNGSI INPUT USER"""
+    panjang = int(input("Masukan nilai panjang : "))
+    lebar = int(input("Masukan nilai lebar : "))
+    return panjang,lebar
+
+def hitung_luas(panjang,lebar) :
+    """FUNGSI LUAS"""
+    return panjang*lebar
+
+def hitung_keliling(panjang,lebar) :
+    """FUNGSI KELILING"""
+    return 2*(panjang+lebar)
+
+def display(message,value) :
+    """FUNGSI DISPLAY"""
+    print(f"Hasil perhitungan {message} = {value}")
+
+# Program utama
+while True :
+    header()
+    PANJANG,LEBAR = input_user()
+    LUAS = hitung_luas(PANJANG,LEBAR)
+    KELILING = hitung_keliling(PANJANG,LEBAR)
+    display("luas", LUAS)
+    display("keliling", KELILING)
+    
+    isContinue = input("Apakah lanjut (y/n)?")
+    if isContinue == "n" :
+        break
+
+print("Program selesai, terima kasih")
