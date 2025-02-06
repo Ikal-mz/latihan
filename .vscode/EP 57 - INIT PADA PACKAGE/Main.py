72cd0288ebfcdda9b8f0716e380b3f044589042f
Uda Mz <mzulfikarb11@gmail.com>
2025-02-06 16:08:22 +0900

    57 init package

---
+++ .vscode/EP 57 - INIT PADA PACKAGE/Main.py
@@ -0,0 +1,25 @@
+import sains
+from sains.matematika import scientific
+
+hasil_tambah = sains.matematika.tambah(1,2,3,4,5,6,7,)
+print(f'hasil jumlah : {hasil_tambah}')
+
+hasil_fisika = sains.fisika.gaya(6,7.8)
+print(f'hasil jumlah : {hasil_fisika}')
+
+hasil_kali = sains.matematika.kali(1,2,6,7.8)
+print(f'hasil jumlah : {hasil_kali}')
+
+pangkat4 = scientific.pangkat(4)
+print(f'hasil pangkat 4 : {pangkat4(3)}')
+# from sains import *
+
+
+# hasil_tambah = matematika.basic.tambah(1,2,3,4,5,6,7,)
+# print(f'hasil jumlah : {hasil_tambah}')
+
+# hasil_fisika = fisika.gaya(6,7.8)
+# print(f'hasil jumlah : {hasil_fisika}')
+
+# hasil_kali = matematika.basic.kali(1,2,6,7.8)
+# print(f'hasil jumlah : {hasil_kali}')
