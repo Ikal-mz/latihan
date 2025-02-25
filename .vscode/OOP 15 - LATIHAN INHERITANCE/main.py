44e445d10c03a7d71dcd31b2087eb036fe72bb44
Uda Mz <mzulfikarb11@gmail.com>
2025-02-25 12:44:31 +0900

    oop 15 - latihan inheritance

---
+++ .vscode/OOP 15 - LATIHAN INHERITANCE/main.py
@@ -0,0 +1,13 @@
+from Hero import Hero_tank,Hero_mage
+
+lolita = Hero_tank('lolita')
+nana = Hero_mage('nana')
+
+lolita.show_info()
+nana.show_info()
+
+lolita.gainExp = 200
+nana.gainExp = 150
+
+lolita.show_info()
+nana.show_info()
